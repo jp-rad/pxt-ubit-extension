b4cac789aa379572ed2390cb30f73c9612e38aa2
jp-rad <jp-rad@example.com>
2024-01-29 04:35:09 +0000

    micro:bit Message Bus ID

--- shims.d.ts
+++ shims.d.ts
@@ -1,12 +1,3 @@
 // Auto-generated. Do not edit.
-declare namespace custom {
-
-    /**
-     * Block definition from C++, no implementation for simulator
-     * https://makecode.com/simshim
-     */
-    //% block shim=custom::bar
-    function bar(): int32;
-}
 
 // Auto-generated. Do not edit. Really.
